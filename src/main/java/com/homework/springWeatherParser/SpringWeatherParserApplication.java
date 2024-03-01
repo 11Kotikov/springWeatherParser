7d9efc59854f4897c0a41216f9d1efad94c7a594
Kotikov <11deadsluts@gmail.com>
2024-03-01 22:10:21 +0300

    Initial commit

---
+++ src/main/java/com/homework/springWeatherParser/SpringWeatherParserApplication.java
@@ -0,0 +1,13 @@
+package com.homework.springWeatherParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeatherParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeatherParserApplication.class, args);
+	}
+
+}
